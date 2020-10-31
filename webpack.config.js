5535aed0dc8fbf35a3f661355bc45f3da47ccf5c
rain <rain@pop-os.localdomain>
2020-10-31 17:45:01 +0700

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+module.exports = [
+    {        
+        entry: "./src/main.scss",
+        output: {
+            path: path.resolve(__dirname,"static/js"),
+            // This is necessary for webpack to compile, but we won't reference it.
+            filename: "style-bundle-main.js",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'bundle-main.css',
+                                outputPath: '../css',
+                            },
+                        },
+                        {
+                            loader: 'extract-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,    
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                    sassOptions: {
+                                        includePaths: ['./node_modules'],
+                                    }
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.ttf$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: '../fonts'
+                            },
+                        },
+                        {
+                            loader: 'extract-loader'
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+];

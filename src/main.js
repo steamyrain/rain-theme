1357a87af3796eac5d5371870ea96be9a5ac4530
steamyrain <reinhard.aditya@gmail.com>
2020-11-05 22:17:07 +0700

    add transition

--- src/main.js
+++ src/main.js
@@ -10,5 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
     const topAppBar = new MDCTopAppBar(topAppBarElement);
     topAppBar.setScrollTarget(document.getElementById('content'));
 
-    // add smooth scroll
+    // default box shadow for app bar
+    document.documentElement.style.setProperty('--o',0)
+
+    // onscroll function
+    window.onscroll = function() {elevateAppBar()};
+
+    function elevateAppBar() {
+        // box shadow opacity transition
+        var appBarHeight = 0;
+        var smMin1 = window.matchMedia("(max-width: 599px)");
+        var sm = window.matchMedia("(min-width: 600px)");
+        if (smMin1.matches) {
+            appBarHeight = 56;
+        } else if (sm.matches) {
+            appBarHeight = 64;
+        }
+        if (window.pageYOffset > 0) {
+            document.documentElement.style.setProperty('--o',1);
+        } else {
+            document.documentElement.style.setProperty('--o',0);
+        }
+    }
+ 
+    // TODO: add smooth scroll
 });

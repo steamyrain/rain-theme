524c1d10d66ea9061128e7a8c9dbbd14389a3717
steamyrain <reinhard.aditya@gmail.com>
2020-11-05 19:17:27 +0700

    fix padding for content div

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCRipple} from '@material/ripple';
 
 document.addEventListener("DOMContentLoaded", function() {
-    const content = document.getElementById("content")
+    // add ripple to button
+    MDCRipple.attachTo(document.querySelector('.mdc-button'));
+
+    // add scroll target
+    const topAppBarElement = document.querySelector('.mdc-top-app-bar');  
+    const topAppBar = new MDCTopAppBar(topAppBarElement);
+    topAppBar.setScrollTarget(document.getElementById('content'));
+
+    // add smooth scroll
 });
